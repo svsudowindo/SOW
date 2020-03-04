09b52d7b0683c7242156b5940e0dba6b5b595ac3
sai korthivada <sai.korthivada@xcubelabs.com>
2020-03-04 20:32:13 +0530

    Changed side bar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouteReuseStrategy } from "@angular/router";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
-import { SplashScreen } from "@ionic-native/splash-screen/ngx";
-import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { AdminLayoutComponent } from "./shared/components/layouts/admin-layout/admin-layout.component";
-import { AuthLayoutComponent } from "./shared/components/layouts/auth-layout/auth-layout.component";
-import { SharedModule } from "./shared/shared.module";
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { HeaderInterceptorsService } from "./shared/services/http/header-interceptors.service";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AdminLayoutComponent } from './shared/components/layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './shared/components/layouts/auth-layout/auth-layout.component';
+import { SharedModule } from './shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeaderInterceptorsService } from './shared/services/http/header-interceptors.service';
 
 @NgModule({
   declarations: [AppComponent, AuthLayoutComponent, AdminLayoutComponent],
